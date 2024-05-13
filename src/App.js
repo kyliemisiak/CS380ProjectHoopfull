f27fcb44aea496105d71fa90da7aaeb62f6e9568
Vicente Garcia <GarciaBarbV@cwu.edu>
2024-05-12 18:57:56 -0700

    Made a rough home page and a map page with working navigation.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route, Navigate, Router, useNavigate, Link } from 'react-router-dom';
 import './App.css';
+import Home from './Home'
+import Map from './Map'
+import { IconButton } from "rsuite";
+import {Explore} from '@rsuite/icons';
+import "rsuite/dist/rsuite.min.css";
+
+/*Go to the map page when clicked*/
+
 
 function App() {
+
+  const ButtonStyle = { margin: "0px 10px" };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className ="App-header">HoopFull Tournament
       </header>
+          <Routes>
+            /*need to specify the path to ech file and imort them*/
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/map" element={<Map/>}></Route>
+          </Routes>
+      <footer className = "App-footer">
+        /*button for map without icon trying to figure*/
+          <Link to="/map">Map</Link>
+      </footer>
     </div>
+    
   );
 }
 
