f525d8633bf2f9cad9b9b8154bb2e8f6013355bd
Vicente Garcia <GarciaBarbV@cwu.edu>
2024-05-19 12:50:58 -0700

    Made adjustemts to the webpages to look nicer.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,47 @@
-import { BrowserRouter, Routes, Route, Navigate, Router, useNavigate, Link } from 'react-router-dom';
+import React from 'react'
+import { Routes, Route, Link } from 'react-router-dom';
+import MenuIcon from '@rsuite/icons/Menu';
+import ExploreIcon from '@rsuite/icons/Explore'
+import PageIcon from '@rsuite/icons/Page';
+import SearchIcon from '@rsuite/icons/Search';
+import { Dropdown, IconButton } from 'rsuite';
 import './App.css';
 import Home from './Home'
 import Map from './Map'
-import { IconButton } from "rsuite";
-import {Explore} from '@rsuite/icons';
 import "rsuite/dist/rsuite.min.css";
-
-/*Go to the map page when clicked*/
-
 
 function App() {
 
-  const ButtonStyle = { margin: "0px 10px" };
+  const NavLink = React.forwardRef(({ href, children, ...rest }, ref) => (
+    <Link ref={ref} to={href} {...rest}>
+      {children}
+    </Link>
+  ));
+
+  const renderIconButton = (props, ref) => {
+    return (
+      <IconButton {...props} ref={ref} icon={<MenuIcon />} padding-left = '20px' color="orange" appearance="primary" />
+    );
+  };
+  
+  
   return (
     <div className="App">
-      <header className ="App-header">HoopFull Tournament
+      <header className ="App-header">
+      <Dropdown renderToggle={renderIconButton} >
+          <Dropdown.Item as={NavLink} href ="/" icon={<PageIcon />}>
+            Home
+          </Dropdown.Item>
+          <Dropdown.Item as={NavLink} href ="/map" icon={<ExploreIcon />}>
+            map
+          </Dropdown.Item>
+        </Dropdown>
       </header>
           <Routes>
             /*need to specify the path to ech file and imort them*/
             <Route path="/" element={<Home/>}></Route>
             <Route path="/map" element={<Map/>}></Route>
           </Routes>
-      <footer className = "App-footer">
-        /*button for map without icon trying to figure*/
-          <Link to="/map">Map</Link>
-      </footer>
     </div>
     
   );
