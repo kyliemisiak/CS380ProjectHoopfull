392c15f9e867721db8d2b4505db1fb54e547e531
NoahPugzley <ntrodal@gmail.com>
2024-05-18 21:11:14 -0700

    Merge branch 'main' of https://github.com/kyliemisiak/CS380ProjectHoopfull

---
+++ src/Home.js
@@ -0,0 +1,167 @@
+function Home() {
+    return (
+      <div className="Home">
+        <p1>this is something new,
+        the casper slide part two,
+        featurin the platinum band,
+        and this time were gonna get funky, funky,
+        everybody clap ur hands, clap clap clap clap ur hands,
+        clap clap clap clap ur hands,
+        alrite now were gonna do the basic step,
+        to the left,
+        take it back now y′all,
+        one hop this time,
+        right foot lets stomp,
+        left foot lets stomp,
+        cha cha real smooth,
+        turn it out,
+        to the left,
+        take it back now y'all
+        one hop this time,
+        right foot lets stomp,
+        left foot lets stomp,
+        cha cha now y′all
+        now its time to get funky
+        to the right now,
+        to the left,
+        take it back now y'all
+        one hop this time,
+        one hop this time,
+        right foot two stomps,
+        left foot two stomps
+        slide to the left,
+        slide to the right
+        criss cross,
+        criss cross,
+        cha cha real smooth
+        lets go to work
+        to the left
+        take it back now y'all
+        two hops this time
+        two hops this time
+        right foot 2 stomps
+        left foot 2 stomps
+        hands on ur knees hands on ur knees
+        get funky with it
+        oooooooh yea
+        come on
+        cha cha now y′all
+        turn it out
+        to the left
+        take it back now y′all
+        5 hops this time
+        hop it out now
+        right foot lets stomp
+        left foot lets stomp
+        right foot again
+        left foot again
+        right foot lets stomp
+        left foot lets stomp
+        freeze everybody clap ur hands
+        come on y'all
+        check it out y′all
+        how low can u go
+        can u go down low
+        all the way to the floor
+        how low can u go
+        can u bring it to the top
+        like u neva gona stop
+        can u bring it to the top
+        one hop
+        right foot now
+        left foot now yall
+        cha cha real smooth
+        turn it out
+        to the left
+        take it back now y'all
+        one hop this time
+        one hop this time
+        reverse
+        reverse
+        slide to the left
+        slide to the right
+        reverse reverse
+        reverse reverse
+        cha cha now y′all
+        cha cha again
+        cha cha now y'all
+        cha cha again
+        turn it out
+        to the left
+        take it back now y′all
+        2 hops 2 hops
+        2 hops 2 hops
+        right foot lets stomp
+        left foot lets stomp
+        charlie brown
+        hop it out now
+        slide to the right
+        slide to the left
+        take it back now y'all
+        cha cha now y'all
+        oh yeah
+        yeah yeah
+        do that stuff
+        oh yeah
+        im outta here y′all
+        peace</p1>
+        <p1>I wanna be the very best
+        Like no one ever was
+        To catch them is my real test
+        To train them is my cause
+        I will travel across the land
+        Searching far and wide
+        Teach Pokemon to understand
+        The power that's inside
+        Pokemon!
+        Gotta catch 'em all--
+        It's you and me
+        I know it's my destiny
+        Pokemon!
+        Ooooh, you're my best friend
+        In a world we must defend
+        Pokemon!
+        Gotta catch 'em all--
+        Our hearts so true
+        Our courage will pull us through
+        You teach me and I'll teach you
+        Pokemon!
+        Gotta catch 'em all!
+        Gotta catch 'em all!
+        Yeeaa.
+        Every challenge along the way
+        With courage I can face
+        I will battle everyday
+        To claim my rightful place
+        Come with me the time is right
+        There's no better team
+        Arm in arm we'll win the fight
+        It's always been our dream
+        Pokemon!
+        Gotta catch 'em all--
+        It's you and me
+        I know it's my destiny
+        Pokemon!
+        Ooooh, you're my best friend
+        In a world we must defend
+        Pokemon!
+        Gotta catch 'em all--
+        Our hearts so true
+        Our courage will pull us through
+        You teach me and I'll teach you
+        Pokemon!
+        Gotta catch 'em all!
+        Gotta catch 'em all!
+
+        Gotta catch 'em all!
+
+        Gotta catch 'em all!
+
+        Gotta catch 'em all!
+
+        Yeeeaa! </p1>
+      </div>
+    );
+  }
+  
+  export default Home;
---
+++ src/Map.js
@@ -0,0 +1,15 @@
+
+
+function Map() {
+    return (
+      <div className="Map">
+        <header>MAp</header>
+        <body>
+            <img src={'https://i.redd.it/rjbb2f83jkl71.jpg'} alt="My Image" />
+            <p>Heeloooooo this should be an image</p>
+          </body>
+      </div>
+    );
+  }
+  
+  export default Map;
