bea96d1020fefb9a30e01b68258cecef26cd48f4
Mariatu Wurie <mariatuwurie@gmail.com>
2024-05-19 21:33:09 -0700

    Commit test

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,5 +1,14 @@
-export const Login = () =>{
+import React,{useState} from "react";
+
+export const Login = () => {
+    const {
     return (
-        <>Login</>
+        <form>
+            <label for="email">email</label>
+            <input type="email" placeholder=" YourAddress@email.com" id="email" name="email"/>
+            <label for="password">password</label>
+            <input type="password" placeholder=" *********" id="password" name="password"/>
+            <button>Login</button>
+        </form>
     )
 }
