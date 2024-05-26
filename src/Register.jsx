b87520583d3d01c1085064271b4adbee384cbfc9
wurie_m <mariatuwurie@gmail.com>
2024-05-26 05:07:39 -0700

    change the register codes a bit

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,29 +1,29 @@
 import "./Register.css"; // Import CSS file
-import React, {useState} from "react";
-export const Register = (props)=> {
+import React, { useState } from "react";
 
+export const Register = (props) => {
     const [email, setEmail] = useState('');
     const [password, setPass] = useState('');
     const [name, setName] = useState("");
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        // Here you can implement your login logic
-        console.log('Logging in with:', {email, password});
+        // Here you can implement your register logic
+        console.log('Registering with:', { email, password, name });
     }
 
-
-    return  (
-        <>
+    return (
+        <div className="register-container">
             <form className="register-form" onSubmit={handleSubmit}>
                 <div className="auth-form-container">
                     <label htmlFor="name"> Full Name</label>
                     <input
-                        type="email"
+                        type="text"
                         value={name}
                         id="name"
                         name="name"
                         placeholder="John Doe"
+                        onChange={(e) => setName(e.target.value)}
                     />
                 </div>
                 <div>
@@ -33,14 +33,17 @@ export const Register = (props)=> {
                         id="password"
                         name="password"
                         placeholder="*******"
-
+                        onChange={(e) => setPass(e.target.value)}
                     />
                 </div>
-                <button type="Log In">Login</button>
+                <button type="submit">Register</button>
             </form>
-            <label htmlFor="button">Already Have An Account </label>
-            <button onClick={() => props.onFormSwitch('login')}>Login Here</button>
-        </>
+            <div className="login-link">
+                <label htmlFor="button">Already Have An Account </label>
+                <button onClick={() => props.onFormSwitch('login')}>Login Here</button>
+            </div>
+        </div>
     );
 }
+
 export default Register;
