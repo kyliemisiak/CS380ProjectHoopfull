023f7d707a45bc36886612f9dd4b866a39893ddc
wurie_m <mariatuwurie@gmail.com>
2024-05-26 00:27:21 -0700

    testing

--- src/App.js
+++ src/App.js
@@ -11,8 +11,7 @@ import Home from './Home'
 import Map from './Map'
 import Teams from './Teams'
 import "rsuite/dist/rsuite.min.css";
-import {Login} from "./Login";
-import {Register} from "./Register";
+
 
 function App() {
 
@@ -50,7 +49,6 @@ function App() {
         <Route path="/map" element={<Map />}></Route>
         <Route path="/teams" element={<Teams />}></Route>
       </Routes>
-      <Login />
     </div>
 
   );
