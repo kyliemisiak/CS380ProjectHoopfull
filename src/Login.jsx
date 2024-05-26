53188de1ff7187a856361748a77bcb95ec836e52
wurie_m <mariatuwurie@gmail.com>
2024-05-26 02:12:53 -0700

    liked buttons to diff pages

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,14 +1,45 @@
-import React,{useState} from "react";
+import React, { useState } from 'react';
 
-export const Login = () => {
-    const {
+export const Login = (props) => {
+    const [email, setEmail] = useState('');
+    const [password,setPass] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Here you can implement your login logic
+        console.log('Logging in with:', { email, password });
+    }
+
     return (
-        <form>
-            <label for="email">email</label>
-            <input type="email" placeholder=" YourAddress@email.com" id="email" name="email"/>
-            <label for="password">password</label>
-            <input type="password" placeholder=" *********" id="password" name="password"/>
-            <button>Login</button>
-        </form>
-    )
-}
+        <div>
+            <h2>Login Page</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="email">Email: </label>
+                    <input
+                        type="email"
+                        value={email}
+                        id="email"
+                        name="email"
+                        placeholder="youremail@gmail.com"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password: </label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="*******"
+
+                    />
+                </div>
+                <button type="Log In">Login</button>
+            </form>
+            <label htmlFor="button">Don't Have An Account?? </label>
+            <button onClick={() => props.onFormSwitch('register')}>Register Here</button>
+        </div>
+    );
+};
+
+export default Login;
--- src/Register.jsx
+++ src/Register.jsx
@@ -1,5 +1,45 @@
-export const Register = () =>{
-    return (
-    <>Register</>
-)
+import React, {useState} from "react";
+export const Register = (props)=> {
+
+    const [email, setEmail] = useState('');
+    const [password, setPass] = useState('');
+    const [name, setName] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Here you can implement your login logic
+        console.log('Logging in with:', {email, password});
+    }
+
+
+    return  (
+        <>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="name"> Full Name</label>
+                    <input
+                        type="email"
+                        value={name}
+                        id="name"
+                        name="name"
+                        placeholder="John Doe"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password: </label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="*******"
+
+                    />
+                </div>
+                <button type="Log In">Login</button>
+            </form>
+            <label htmlFor="button">Already Have An Account </label>
+            <button onClick={() => props.onFormSwitch('login')}>Login Here</button>
+        </>
+    );
 }
+export default Register;
